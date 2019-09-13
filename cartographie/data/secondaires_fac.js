bbefcca26f216313cd621a3370a2e39c7aba7171
JBD-8 <52410559+JBD-8@users.noreply.github.com>
2019-09-13 16:40:03 +0200

    Projet DURB

---
+++ cartographie/data/secondaires_fac.js
@@ -0,0 +1,11 @@
+var sec_fac = {
+"type": "FeatureCollection",
+"name": "secondaires_fac",
+"features": [
+{ "type": "Feature", "properties": { "Facade": null, "NB_SEC_1999": 1537512, "NB_SEC_2011": 1573891, "NB_SEC_2016": 1704433, "NB_LOG_1999": 20125150, "NB_LOG_2011": 22924657, "NB_LOG_2016": 24097417 }, "geometry": null },
+{ "type": "Feature", "properties": { "Facade": "Atlantique Nord - Manche Ouest", "NB_SEC_1999": 352479, "NB_SEC_2011": 396189, "NB_SEC_2016": 422708, "NB_LOG_1999": 2260955, "NB_LOG_2011": 2711367, "NB_LOG_2016": 2892527 }, "geometry": null },
+{ "type": "Feature", "properties": { "Facade": "Atlantique Sud", "NB_SEC_1999": 194151, "NB_SEC_2011": 235298, "NB_SEC_2016": 253246, "NB_LOG_1999": 1182028, "NB_LOG_2011": 1442988, "NB_LOG_2016": 1565423 }, "geometry": null },
+{ "type": "Feature", "properties": { "Facade": "Méditerranée", "NB_SEC_1999": 647957, "NB_SEC_2011": 738631, "NB_SEC_2016": 799018, "NB_LOG_1999": 3456419, "NB_LOG_2011": 4024598, "NB_LOG_2016": 4279676 }, "geometry": null },
+{ "type": "Feature", "properties": { "Facade": "Manche Est - Mer du Nord", "NB_SEC_1999": 199562, "NB_SEC_2011": 212687, "NB_SEC_2016": 227032, "NB_LOG_1999": 2280211, "NB_LOG_2011": 2568368, "NB_LOG_2016": 2699293 }, "geometry": null }
+]
+}
---
+++ cartographie/data/secondaires_fra.js
@@ -0,0 +1,7 @@
+var sec_fra = {
+"type": "FeatureCollection",
+"name": "secondaires_fra",
+"features": [
+{ "type": "Feature", "properties": { "Expr": "France", "NB_SEC_1999": 2931661, "NB_SEC_2011": 3156696, "NB_SEC_2016": 3406437, "NB_LOG_1999": 29304763, "NB_LOG_2011": 33671978, "NB_LOG_2016": 35534336 }, "geometry": null }
+]
+}
